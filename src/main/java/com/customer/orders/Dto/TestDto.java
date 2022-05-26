05d48b7c13de7dfd557f7343efa00d35b6af6e48
Your Name <you@example.com>
2022-05-26 15:38:02 +0530

    second commit

---
+++ src/main/java/com/customer/orders/Dto/TestDto.java
@@ -0,0 +1,15 @@
+package com.customer.orders.Dto;
+
+public class TestDto {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
